a25f5ed3aa3e90a7410776c199e1830889da647a
fady <fadyyoussef4444@gamil.com>
2025-12-02 00:35:06 +0200

    feat: test fcfs in the main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,46 @@
 package org.example;
 
+import org.example.FirstComeFirstServe.FirstComeFirstServeAlgorithm;
 import org.example.roundrobin.RoundRobinAlgorithm;
 import org.example.sjf.SJFAlgorithm;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Generator generator = new Generator();
+        System.out.println("1.FCFS\n2.SJF\n3.SRT\n4.Round Robin\n5.Priority scheduling\n6.Multi level scheduling\n7.Multi level Feedback scheduling");
+        Scanner input = new Scanner(System.in);
+        List<Process> processList = generator.getProcessList();
+        SchedulingAlgorithm schedulingAlgorithm;
+        List<String> result;
+        switch (input.nextInt()) {
+            case 1:
+                ///  put your code here.
+                ///  Naming Convention -> (Algorithm name)List, (Algorithm name)Result
 
-        ///  put your code here.
-        ///  Naming Convention -> (Algorithm name)List, (Algorithm name)Result
-//        List<Process> roundRobinList = generator.getProcessList();
-//        SchedulingAlgorithm schedulingAlgorithm = new RoundRobinAlgorithm(roundRobinList);
-//        List<String> roundRobinResult = schedulingAlgorithm.run(roundRobinList);
+                schedulingAlgorithm = new FirstComeFirstServeAlgorithm(processList);
+                result = schedulingAlgorithm.run(processList);
+                break;
+            case 2:
+                schedulingAlgorithm = new SJFAlgorithm(processList);
+                result = schedulingAlgorithm.run(processList);
+                break;
+            case 4:
+                ///  put your code here.
+                ///  Naming Convention -> (Algorithm name)List, (Algorithm name)Result
+                schedulingAlgorithm = new RoundRobinAlgorithm(processList);
+                result = schedulingAlgorithm.run(processList);
+                break;
+            default:
+                System.out.println("Please, Enter a valid input");
+                return;
+        }
 
-        List<Process> sjfList = generator.getProcessList();
-        SchedulingAlgorithm schedulingAlgorithm = new SJFAlgorithm(sjfList);
-        List<String> sjfResult = schedulingAlgorithm.run(sjfList);
 
         ///  Print Result here
-        schedulingAlgorithm.print(sjfResult);
+        schedulingAlgorithm.print(result);
         schedulingAlgorithm.printStatistics();
     }
 }
